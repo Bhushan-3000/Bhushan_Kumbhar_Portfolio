2ba63950076f1064702998fbbd86c6eea31a828a
Bhushan-3000 <134437390+Bhushan-3000@users.noreply.github.com>
2026-02-07 16:08:53 +0530

    update profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import profileImg from "./assets/profile.JPG";
+import profileImg from "./assets/profile.png";
 
 import { motion, AnimatePresence } from "framer-motion";
 import {
